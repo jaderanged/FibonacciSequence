03d0672f7734214515b0340e02615ac5620fa969
Jade Boone <36799515+jaderanged@users.noreply.github.com>
2023-01-22 15:45:34 -0500

    Add files via upload
    fib sequence main file

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.print("The " + n + "th term of the Fibonacci sequence is: ");
+
+        for(int i = 0; i < n; i++){
+            System.out.print(fib(i) + " ");
+        }
+    }
+
+
+    public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
